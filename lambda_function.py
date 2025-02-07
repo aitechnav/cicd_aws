d411ae5fa9dc793376ea35d1218102ad5b6f36fe
tyagian <tyagi.an@northeastern.edu>
2025-02-06 20:55:06 -0500

    fix wsgi middleware

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
+from aws_wsgi_middleware import WSGIMiddleware
 from app.main import app
-from awsgi import WSGIMiddleware
 
 def lambda_handler(event, context):
     return WSGIMiddleware(app).handle(event, context)
